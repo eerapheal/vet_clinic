0b5166293c65c89214eac6e1e64f2a03e0014c8a
unknown <ekpenisiraphael@gmail.com>
2022-12-17 13:12:57 +0530

    Join table

--- data.sql
+++ data.sql
@@ -2,17 +2,23 @@
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES 
-('Agumon', '2020-02-03', 0, 'TRUE', 10.23),
-( ='Gabumon', '2018-11-15', 2, 'TRUE', 8),
-('Pikachu', '2021-01-07', 1, 'FALSE', 15.04),
-('Devimon', '2017-05-12', 5, 'TRUE', 11);
+(1, 'Agumon', '2020-02-03', 0, 'TRUE', 10.23),
+(2, 'Gabumon', '2018-11-15', 2, 'TRUE', 8),
+(3, 'Pikachu', '2021-01-07', 1, 'FALSE', 15.04),
+(4, 'Devimon', '2017-05-12', 5, 'TRUE', 11);
+(5, 'Charmander', '2020-02-08', 0, 'FALSE', 11),
+(6, 'Plantmon', '2021-11-15', 2, 'TRUE', 5.7),
+(7, 'Squirtle', '1993-04-02', 3, 'FALSE', 12.13),
+(8, 'Angemon', '2005-06-12', 1, 'TRUE', 45),
+(9, 'Boarmon', '2005-06-07', 7, 'TRUE', 20.4),
+(10, 'Blossom', '1998-10-13', 3, 'TRUE', 17),
 
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES 
-('Charmander', '2020-02-08', 0, 'FALSE', -11),
-('Plantmon', '2021-11-15', 2, 'TRUE', -5.7),
-('Squirtle', '1993-04-02', 3, 'FALSE', -12.13),
+('Charmander', '2020-02-08', 0, 'FALSE', 11),
+('Plantmon', '2021-11-15', 2, 'TRUE', 5.7),
+('Squirtle', '1993-04-02', 3, 'FALSE', 12.13),
 ('Angemon', '2005-06-12', 1, 'TRUE', -45),
 ('Boarmon', '2005-06-07', 7, 'TRUE', 20.4),
 ('Blossom', '1998-10-13', 3, 'TRUE', 17),
@@ -67,4 +73,44 @@ SET owner_id =
 UPDATE animals
 SET owner_id = 
     (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
-    WHERE name IN ('Angemon', 'Boarmon');    
+    WHERE name IN ('Angemon', 'Boarmon'); 
+
+/*jOIN TABLE*/
+    INSERT INTO vets (name, age, date_of_graduation) 
+VALUES 
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');    
+
+
+INSERT INTO specializations (vets_id, species_id) 
+VALUES 
+(1, 1),
+(3, 2),
+(3, 1),
+(4, 2);
+
+
+INSERT INTO visits (animal_id, vet_id, date_of_visit) 
+VALUES 
+(5, 1, '2020-05-24'),
+(5, 3, '2020-07-22'),
+(6, 4, '2021-02-02'),
+(9, 2, '2020-01-05'),
+(9, 2, '2020-03-08'),
+(9, 2, '2020-05-14'),
+(7, 3, '2021-05-04'),
+(3, 4, '2021-02-24'),
+(1, 2, '2019-12-21'),
+(1, 1, '2020-08-10'),
+(1, 2, '2021-04-07'),
+(4, 3, '2019-09-29'),
+(2, 4, '2020-10-03'),
+(2, 4, '2020-11-04'),
+(8, 2, '2019-01-24'),
+(8, 2, '2019-05-15'),
+(8, 2, '2020-02-27'),
+(8, 2, '2020-08-03'),
+(10, 3, '2020-05-24'),
+(10, 1, '2021-01-11');   
